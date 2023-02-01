74f20303f7da612861de74446c8b6a1ce4bf38ae
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-02-01 09:54:09 -0300

    feat/added choiceSchema

---
+++ src/schemas/choiceSchema.js
@@ -0,0 +1,8 @@
+import Joi from 'joi'
+
+const choiceSchema = Joi.object({
+    title: Joi.string().min(1).required(),
+    pollId: Joi.string().regex(/^[a-f\d]{24}$/i)
+})
+
+export default choiceSchema
